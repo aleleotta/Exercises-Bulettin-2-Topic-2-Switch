da5b3fbff985615e13b8750c6dbe7daf66c114d0
aleleotta <aleleotta@106-13.info2>
2022-10-11 12:51:15 +0200

    Exercise 4 in progress!

--- Exercises Bulettin 2 Topic 2 Switch/src/exercises/Exercise4.java
+++ Exercises Bulettin 2 Topic 2 Switch/src/exercises/Exercise4.java
@@ -3,10 +3,13 @@ import java.util.*;
 
 public class Exercise4 {
 	public static void main(String[] args) {
+		int firstDice, secondDice;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Roll the dices.");
 		System.out.println("What number did the first dice get: ");
+		firstDice = sc.nextInt();
 		System.out.println("What number did the second dice get: ");
+		secondDice = sc.nextInt();
 		sc.close();
 	}
 }
