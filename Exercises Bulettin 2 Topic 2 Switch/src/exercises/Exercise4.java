5e638694a94e70a079e5fcc40bcaa3be5d9f0766
aleleotta <aleleotta@106-13.info2>
2022-10-13 13:51:58 +0200

    Exercise 4 complete but still needs to be documented.

--- Exercises Bulettin 2 Topic 2 Switch/src/exercises/Exercise4.java
+++ Exercises Bulettin 2 Topic 2 Switch/src/exercises/Exercise4.java
@@ -3,13 +3,66 @@ import java.util.*;
 
 public class Exercise4 {
 	public static void main(String[] args) {
-		int firstDice, secondDice;
+		String firstDice, secondDice;
+		int firstDice1, secondDice2, result;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Roll the dices.");
 		System.out.println("What number did the first dice get: ");
-		firstDice = sc.nextInt();
+		firstDice = sc.next();
 		System.out.println("What number did the second dice get: ");
-		secondDice = sc.nextInt();
+		secondDice = sc.next();
+		firstDice1 = switch (firstDice) {
+			case "ONE" -> {
+				yield 1;
+			}
+			case "TWO" -> {
+				yield 2;
+			}
+			case "THREE" -> {
+				yield 3;
+			}
+			case "FOUR" -> {
+				yield 4;
+			}
+			case "FIVE" -> {
+				yield 5;
+			}
+			case "SIX" -> {
+				yield 6;
+			}
+			default -> {
+				System.out.println("ERROR!");
+				yield -1;
+			}
+		};
+		secondDice2 = switch (secondDice) {
+			case "ONE" -> {
+				yield 1;
+			}
+			case "TWO" -> {
+				yield 2;
+			}
+			case "THREE" -> {
+				yield 3;
+			}
+			case "FOUR" -> {
+				yield 4;
+			}
+			case "FIVE" -> {
+				yield 5;
+			}
+			case "SIX" -> {
+				yield 6;
+			}
+			default -> {
+				System.out.println("ERROR!");
+				yield -1;
+			}
+		};
+		result = firstDice1 + secondDice2;
+		System.out.println("The result for both dice numbers is: " + result);
+		System.out.println("");
+		System.out.println("Have a nice day!");
 		sc.close();
 	}
 }
---
+++ Exercises Bulettin 2 Topic 2 Switch/src/exercises/Exercise5.java
@@ -0,0 +1,8 @@
+package exercises;
+import java.util.*;
+
+public class Exercise5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
