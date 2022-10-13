19ad9efcc7bbb2b3f6f5b53c7d849746a3b918cb
aleleotta <aleleotta@106-13.info2>
2022-10-13 14:06:10 +0200

    Exercise5 needs to get documented.

--- Exercises Bulettin 2 Topic 2 Switch/src/exercises/Exercise5.java
+++ Exercises Bulettin 2 Topic 2 Switch/src/exercises/Exercise5.java
@@ -3,6 +3,36 @@ import java.util.*;
 
 public class Exercise5 {
 	public static void main(String[] args) {
+		String drivingLicense;
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Insert your driving license category: ");
+		drivingLicense = sc.next();
+		switch (drivingLicense) {
+			case "B1" -> {
+				System.out.println("Cars");
+			}
+			case "B2" -> {
+				System.out.println("Cars");
+			}
+			case "A" -> {
+				System.out.println("Motorcycles");
+			}
+			case "C1" -> {
+				System.out.println("Trucks");
+			}
+			case "C5" -> {
+				System.out.println("Trucks");
+			}
+			case "D" -> {
+				System.out.println("Buses");
+			}
+			case "E" -> {
+				System.out.println("Trailers");
+			}
+			default -> {
+				System.out.println("You haven´t inserted a driving license category. Please execute and try again.");
+			}
+		}
+		sc.close();
 	}
 }
